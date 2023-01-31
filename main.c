bf2525bd5747a0eba4c9b7976d6100428ecb94e0
carrot1358 <carrot1358@gmail.com>
2023-01-31 09:15:04 +0700

    try to make it work

--- main.c
+++ main.c
@@ -1,287 +1,183 @@
-/*
-LCD Display
-
-A friend of yours has just bought a new computer. Before this, the most powerful
-machine he ever used was a pocket calculator. He is a little disappointed because he
-liked the LCD display of his calculator more than the screen on his new computer! To
-make him happy, write a program that prints numbers in LCD display style.
-
-Input
-
-The input file contains several lines, one for each number to be displayed. Each line
-contains integers s and n, where n is the number to be displayed (0 ≤ n ≤ 99, 999, 999)
-and s is the size in which it shall be displayed (1 ≤ s ≤ 10). The input will be terminated
-by a line containing two zeros, which should not be processed.
-
-Output
-
-Print the numbers specified in the input file in an LCD display-style using s “-” signs
-for the horizontal segments and s “|” signs for the vertical ones. Each digit occupies
-exactly s + 2 columns and 2s + 3 rows. Be sure to fill all the white space occupied by
-the digits with blanks, including the last digit. There must be exactly one column of
-blanks between two digits.
-Output a blank line after each number. You will find an example of each digit in the
-sample output below.
-
-Sample Input
-
-2 12345
-3 67890
-0 0
-
-Sample Output
-
-      --   --        --
-   |    |    | |  | |
-   |    |    | |  | |
-      --   --   --   --
-   | |       |    |    |
-   | |       |    |    |
-      --   --        --
-
- ---   ---   ---   ---   ---
-|         | |   | |   | |   |
-|         | |   | |   | |   |
-|         | |   | |   | |   |
- ---         ---   ---
-|   |     | |   |     | |   |
-|   |     | |   |     | |   |
-|   |     | |   |     | |   |
- ---         ---   ---   ---
-
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
+#include "stdio.h"
 
 int nLines;
 int nColumns;
+int input[8];
 char lcdNumber[23][12];
 char lcdNumbers[8][23][12];
+int s,n;
 
-void setLcdSize(int size) {
-    nLines = 2 * size + 3;
-    nColumns = size + 2;
+void drawA(){
+    for(int i = 1 ; i <= s ; i++){
+        lcdNumber[0][i] = '-';
+    }
 }
-
-void clearLcd() {
-    int i;
-    for (i = 0; i < nLines; i++) {
-        int j;
-        for (j = 0; j < nColumns; j++) {
-            lcdNumber[i][j] = ' ';
+void drawG(){
+    for(int i = 1 ; i <= s ; i++){
+        lcdNumber[s+1][i] = '-';
+    }
+}
+void drawD(){
+    for(int i = 1 ; i <= s ; i++){
+        lcdNumber[2*s+2][i] = '-';
+    }
+}
+void drawF(){
+    for(int i = 1 ; i <= s ; i++){
+        lcdNumber[i][0] = '|';
+    }
+}
+void drawB(){
+    for(int i = 1 ; i <= s ; i++){
+        lcdNumber[i][s+1] = '|';
+    }
+}
+void drawE(){
+    for(int i = s+2 ; i <= 2*s+1 ; i++){
+        lcdNumber[i][0] = '|';
+    }
+}
+void drawC(){
+    for(int i = s+2 ; i <= 2*s+1 ; i++){
+        lcdNumber[i][s+1] = '|';
+    }
+}
+void clear_digit(){
+    for(int i=0;i<23;i++){ //collumn
+        for(int j=0;j<12;j++){ //row
+            lcdNumber[i][j]=' ';
         }
     }
 }
-
-void drawLine(int lineNumber) {
-    int j;
-    for (j = 1; j < nColumns - 1; j++) {
-        lcdNumber[lineNumber][j] = '-';
+void draw_digit(int n){
+    clear_digit();
+    if(n==0){
+        drawA();
+        drawB();
+        drawC();
+        drawD();
+        drawE();
+        drawF();
+    }
+    else if(n==1){
+        drawB();
+        drawC();
+    }
+    else if(n==2){
+        drawA();
+        drawB();
+        drawG();
+        drawE();
+        drawD();
+    }
+    else if(n==3){
+        drawA();
+        drawB();
+        drawG();
+        drawC();
+        drawD();
+    }
+    else if(n==4){
+        drawF();
+        drawG();
+        drawB();
+        drawC();
+    }
+    else if(n==5){
+        drawA();
+        drawF();
+        drawG();
+        drawC();
+        drawD();
+    }
+    else if(n==6){
+        drawA();
+        drawF();
+        drawG();
+        drawC();
+        drawD();
+        drawE();
+    }
+    else if(n==7){
+        drawA();
+        drawB();
+        drawC();
+    }
+    else if(n==8){
+        drawA();
+        drawB();
+        drawC();
+        drawD();
+        drawE();
+        drawF();
+        drawG();
+    }
+    else if(n==9){
+        drawA();
+        drawB();
+        drawC();
+        drawD();
+        drawF();
+        drawG();
     }
 }
-
-void drawFirstLine() {
-    drawLine(0);
-}
-
-void drawMiddleLine() {
-    drawLine(nLines / 2);
-}
-
-void drawLastLine() {
-    drawLine(nLines - 1);
-}
-
-void drawFirstColumn(int columnNumber) {
-    int i;
-    for (i = 1; i < nLines / 2; i++) {
-        lcdNumber[i][columnNumber] = '|';
-    }
-}
-
-void drawLastColumn(int columnNumber) {
-    int i;
-    for (i = (nLines / 2) + 1; i < nLines - 1; i++) {
-        lcdNumber[i][columnNumber] = '|';
-    }
-}
-
-void drawFirstLeftColumn() {
-    drawFirstColumn(0);
-}
-
-void drawLastLeftColumn() {
-    drawLastColumn(0);
-}
-
-void drawFirstRightColumn() {
-    drawFirstColumn(nColumns - 1);
-}
-
-void drawLastRightColumn() {
-    drawLastColumn(nColumns - 1);
-}
-
-void printLcd(int position) {
-    int i;
-    for (i = 0; i < nLines; i++) {
-        int j;
-        for (j = 0; j < nColumns; j++) {
-            lcdNumbers[position][i][j] = lcdNumber[i][j];
-        }
-    }
-}
-
-void printZero(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstLeftColumn();
-    drawFirstRightColumn();
-    drawLastLeftColumn();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printOne(int position) {
-    clearLcd();
-    drawFirstRightColumn();
-    drawLastRightColumn();
-    printLcd(position);
-}
-
-void printTwo(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstRightColumn();
-    drawMiddleLine();
-    drawLastLeftColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printThree(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstRightColumn();
-    drawMiddleLine();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printFour(int position) {
-    clearLcd();
-    drawFirstLeftColumn();
-    drawFirstRightColumn();
-    drawMiddleLine();
-    drawLastRightColumn();
-    printLcd(position);
-}
-
-void printFive(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstLeftColumn();
-    drawMiddleLine();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printSix(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstLeftColumn();
-    drawMiddleLine();
-    drawLastLeftColumn();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printSeven(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstRightColumn();
-    drawLastRightColumn();
-    printLcd(position);
-}
-
-void printEight(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstLeftColumn();
-    drawFirstRightColumn();
-    drawMiddleLine();
-    drawLastLeftColumn();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printNine(int position) {
-    clearLcd();
-    drawFirstLine();
-    drawFirstLeftColumn();
-    drawFirstRightColumn();
-    drawMiddleLine();
-    drawLastRightColumn();
-    drawLastLine();
-    printLcd(position);
-}
-
-void printNumber(int n, int position) {
-    void (*functions[10]) (int position) = {
-            printZero,
-            printOne,
-            printTwo,
-            printThree,
-            printFour,
-            printFive,
-            printSix,
-            printSeven,
-            printEight,
-            printNine
-    };
-
-    functions[n](position);
-}
-
-int main() {
-    int size = 0;
-    char strNumber[9];
-    scanf("%d %s", &size, strNumber);
-
-    while (size != 0 || atoi(strNumber) != 0) {
-        setLcdSize(size);
-        int length = strlen(strNumber);
-
-        int i;
-        for (i = 0; i < length; i++) {
-            int n = strNumber[i] - '0';
-            printNumber(n, i);
-        }
-
-        for (i = 0; i < nLines; i++) {
-            int n;
-            for (n = 0; n < length; n++) {
-                int j;
-                for (j = 0; j < nColumns; j++) {
-                    printf("%c", lcdNumbers[n][i][j]);
-                }
-                if (n < length - 1) {
-                    printf(" "); //column of blanks
-                }
-            }
-            printf("\n");
+void print_digit() {
+    for (int i = 0; i < 2*s+3; i++) { //collumn
+        for (int j = 0; j < s+2; j++) { //row
+            printf("%c", lcdNumber[i][j]);
         }
         printf("\n");
-
-        scanf("%d %s", &size, strNumber);
     }
-    return 0;
+}
+void n_to_array(int n){
+
+    for(int i = 7 ; i>=0 ; i --)
+    {
+        while(n!=0){
+            input[i]=n%10;
+            n/=10;
+        }
+    }
+}
+void drawIN3D(){
+    for(int i = 0 ; i<=7 ; i++)//position
+    {
+        draw_digit(input[i]);
+        print_digit();
+        printf("\n%d",i);
+        for(int j = 0; j < nLines; j++) { //rows
+            for (int k = 0; k < nColumns; k++) { //columns
+                lcdNumbers[i][j][k] = lcdNumber[j][k];
+            }
+        }
+    }
+}
+void print_digit3D(){
+    for (int i = 0; i < 2*s+3; i++) { //collumn
+        for(int k = 0 ; k<=7 ; k++){
+            for (int j = 0; j < s+2; j++) { //row
+                printf("%c", lcdNumbers[k][i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+void lcdclear3D(){
+    for(int k = 0 ; k<=7 ; k++){
+        for(int i=0;i<23;i++){ //collumn
+            for(int j=0;j<12;j++){ //row
+                lcdNumbers[k][i][j]=' ';
+            }
+        }
+    }
+
+}
+void main(){
+    scanf("%d %d",&s,&n);
+    n_to_array(n);
+    lcdclear3D();
+    drawIN3D();
+    print_digit3D();
+    //draw_digit(n);
+    //print_digit();
 }
